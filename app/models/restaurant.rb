e3dc5663f5a7c366cb5db0cfb4fb89c75d86a33e
brunna monteiro <brunnabrauner@gmail.com>
2023-08-16 14:23:29 +0200

    Added validations rules to the form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :delete_all
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :address, presence: true, length: { minimum: 10 }
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
 end
